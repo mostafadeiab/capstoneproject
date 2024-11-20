940cac243e2a886f33e0b496041f61682803c7f6
Vijayant Mehra <vijayantmehra10@gmail.com>
2024-11-20 15:53:12 -0500

    Demo Addition of data

--- app/demo/page.tsx
+++ app/demo/page.tsx
@@ -19,7 +19,6 @@ type DemoData = {
 export default function Demo() {
   const [staticData, setStaticData] = useState<DemoData[]>([]);
   const [addedData, setAddedData] = useState<DemoData[]>([]);
-  const [addedRowsCount, setAddedRowsCount] = useState(0);
 
   // Load initial data
   useEffect(() => {
@@ -87,12 +86,12 @@ export default function Demo() {
     // Generate random flow rate between 4 and 5.5
     const flowRate = Math.random() * (5.5 - 4) + 4;
     
-    // Generate random volume between 0.8 and 1.12
-    const volume = Math.random() * (1.12 - 0.8) + 0.8;
+    // Generate random volume between 0.1 and 0.3
+    const volume = Math.random() * (0.3 - 0.1) + 0.1;
 
     const newRow: DemoData = {
       'Timestamp': timestamp,
-      'Device Name': 'toilet',
+      'Device Name': 'bathroom_sink',
       'Start Time': time,
       'End Time': endTime,
       'Duration (s)': formatNumber(duration, 1),
@@ -103,12 +102,10 @@ export default function Demo() {
     };
 
     setAddedData(prev => [...prev, newRow]);
-    setAddedRowsCount(prev => prev + 1);
   };
 
   const clearAddedRows = () => {
     setAddedData([]);
-    setAddedRowsCount(0);
   };
 
   return (
@@ -126,7 +123,7 @@ export default function Demo() {
               <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                 <path fillRule="evenodd" d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z" clipRule="evenodd" />
               </svg>
-              Refresh ({addedRowsCount} rows added)
+              Refresh
             </button>
             <button
               onClick={clearAddedRows}
